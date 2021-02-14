1bfcd50c6faca384489612bb6714b7b3a4fe40bf
Mateusz Samek <samekmat@gmail.com>
2021-02-14 20:59:44 +0100

    Model Rezerwacji + meta

--- warsztat/models.py
+++ warsztat/models.py
@@ -5,3 +5,13 @@ class Room(models.Model):
     name = models.CharField(max_length=255, unique=True)
     capacity = models.IntegerField()
     projector_availability = models.BooleanField()
+
+
+class Reservation(models.Model):
+    date = models.DateField()
+    room_id = models.ForeignKey(Room, on_delete=models.CASCADE)
+    comment = models.TextField
+
+
+class Meta:
+    unique_together = ('room_id', 'date')
